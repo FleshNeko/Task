31b63f27502072ccd3a5cb787ef419dfe53c7c73
FleshNeko <102514067+FleshNeko@users.noreply.github.com>
2022-04-10 11:21:06 +0300

    Add files via upload

---
+++ task1/Task1.js
@@ -0,0 +1,6 @@
+const myElement = document.getElementById("pasta");
+myElement.style.background = "#1300E1";
+
+function myFunction() {
+  document.getElementById("recipe").value = "Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many exotic and expensive cheeses, but they aren't always the best tasting. This simple recipe is cheap and tasty.";
+}
---
+++ task2/Task2.js
@@ -0,0 +1,15 @@
+let myElement = document.getElementById("pasta");
+		myElement.style.background = "blue";
+		document.getElementById("pasta").onclick = function () {
+		alert(document.getElementById("pasta").style.backgroundColor);
+}
+let myElement2 = document.getElementById("meat");
+		myElement2.style.background = "red";
+		document.getElementById("meat").onclick = function () {
+		alert(document.getElementById("meat").style.backgroundColor);
+}
+let myElement3 = document.getElementById("cheeze");
+		myElement3.style.background = "orange";
+		document.getElementById("cheeze").onclick = function () {
+		alert(document.getElementById("cheeze").style.backgroundColor);
+}
