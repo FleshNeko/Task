61e8ec9d225b231cf7e46793845cc45ebcc8b391
FleshNeko <102514067+FleshNeko@users.noreply.github.com>
2022-05-08 10:17:31 +0300

    Delete Task1.js

--- Task1.js
+++
@@ -1,8 +0,0 @@
-const pasta = document.getElementById ("pasta");
-console.log(pasta);
-
-const recipe  = document.getElementById ("recipe");
-//recipe.onclick = function () { я не совсем поняла должен ли он меняться по клику или просто другой текст, который записан уже тут 
-document.getElementById("recipe").value= "придумала, что смогла";
-console.log(recipe);
-//}
